d45b0c0fc13fc69df853d3f74b18b8feaa56fb1c
James O'Brien <codinglicensetothrill@gmail.com>
2015-01-28 14:28:02 -0800

    deleting nodejs

---
+++ web/build/build.config.js
@@ -0,0 +1,11 @@
+﻿({    
+    name: 'main', 
+    baseUrl: '../src/scripts', 
+    mainConfigFile: '../src/scripts/main.js', 
+    out: '../src/scripts/main-optimized.min.js', 
+    generateSourceMaps: true, 
+    preserveLicenseComments: false, 
+    optimize: 'uglify2'
+})
+//Use this command to build 
+//node build\r.js -o build\build.config.js
---
+++ web/src/scripts/ui/app.js
@@ -0,0 +1,81 @@
+﻿define(['jquery', 'controller', 'options'], function ($, controller, options) {
+
+    $(document).ready(function () {
+        
+        var optionsSection = $("#optionsSection"),
+            gameSection = $("#gameSection"),
+            topNavigation = $("#topNavigation");                              
+
+        //Initialize UI
+        optionsSection.hide();
+        gameSection.hide();
+        
+
+        //Options Events              
+        $("#btnOptions").on("click", function () {
+
+            showOptions();
+            controller.populateOptions();
+
+        });
+        $("#btnCancelOptions").on("click", hideOptions);
+        $("#btnSaveOptions").on("click", function () {
+
+            hideOptions();
+            controller.saveOptions();
+
+        });
+
+        //BoardGame events
+        $("#btnPlay").on("click", function () {
+            
+            showBoardGame();  
+            controller.playGame();
+
+        });
+        $("#btnQuitGame").on("click", function () {
+
+            hideBoardgame();
+            controller.quitGame();
+            
+        });
+        $("#btnPlayerDeal").on("click", controller.deal);
+        $('#btnPlayerDecreaseBet0, #btnPlayerDecreaseBet1, #btnPlayerDecreaseBet2').on("click", { amount: -1 }, controller.changeBet);
+        $('#btnPlayerIncreaseBet0, #btnPlayerIncreaseBet1, #btnPlayerIncreaseBet2').on("click", { amount: 1 }, controller.changeBet);
+        $('#btnPlayerHit0, #btnPlayerHit1, #btnPlayerHit2').on("click", controller.hit);
+        $('#btnPlayerStand0, #btnPlayerStand1, #btnPlayerStand2').on("click", controller.stand);
+
+
+        // Switching Between UI functionality
+        function showOptions() {
+
+            topNavigation.hide();
+            optionsSection.show();
+
+        }
+
+        function hideOptions() {
+
+            topNavigation.show();
+            optionsSection.hide();
+
+        }
+
+        function hideBoardgame() {
+
+            gameSection.hide();
+            topNavigation.show();                
+
+        }
+        
+        function showBoardGame(){
+
+            gameSection.show();
+            topNavigation.hide();
+
+        }       
+     
+
+    });
+
+});
